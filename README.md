ae48a7d200c7d002f724acd63fbaea7a85014871
lvgalvaofilho <lvgalvaofilho@gmail.com>
2024-06-11 21:51:30 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # estamos ao vivo!
+
+```mermaid
+graph TD;
+    A[Início] --> B[Extrair Dados das Commodities]
+    B --> C[Transformar Dados das Commodities]
+    C --> D[Carregar Dados no PostgreSQL]
+    D --> E[Fim]
+
+    subgraph Extrair
+        B1[Buscar Dados de Cada Commodity]
+        B2[Adicionar Dados na Lista]
+    end
+
+    subgraph Transformar
+        C1[Concatenar Todos os Dados]
+        C2[Preparar DataFrame]
+    end
+
+    subgraph Carregar
+        D1[Salvar DataFrame no PostgreSQL]
+    end
+
+    B --> B1
+    B1 --> B2
+    B2 --> C
+    C --> C1
+    C1 --> C2
+    C2 --> D
+    D --> D1
+
+```
---
+++ app/app.py
@@ -0,0 +1,38 @@
+import os
+import pandas as pd
+import streamlit as st
+from sqlalchemy import create_engine
+from sqlalchemy.exc import ProgrammingError
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Obter as variáveis do arquivo .env
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+# Criar a URL de conexão do banco de dados
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Criar o engine de conexão com o banco de dados
+engine = create_engine(DATABASE_URL)
+
+def get_data():
+    query = f"""
+    SELECT
+        data,
+        simbolo,
+        valor_fechamento,
+        acao,
+        quantidade,
+        valor,
+        ganho
+    FROM
+        public.dm_commodities;
+    """
+    df = pd.read_sql(query, engine)
